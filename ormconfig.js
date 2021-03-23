60349f57e28124ce1ef89fcd09886af3b02969cd
Victor Chagas <victorsantoschagas@hotmail.com>
2021-03-23 19:06:44 -0300

    COMMIT Com deploy parcial c heroku

--- ormconfig.js
+++ ormconfig.js
@@ -4,10 +4,10 @@ module.exports = [
         "type":"postgres",
         "url": process.env.DATABASE_URL,
         "entities": [
-            "./dist/src/modules/**/infra/typeorm/entities/*.ts"
+            "dist/src/modules/**/infra/typeorm/entities/*.ts"
           ],
         "migrations":[
-            "./dist/src/shared/infra/typeorm/migrations/*.ts"
+            "dist/src/shared/infra/typeorm/migrations/*.ts"
         ],
         "cli":{
             "migrationsDir": "./src/shared/typeorm/migrations"
