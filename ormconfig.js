80d6830fc56d89fc6758456918b58a3a54140deb
Victor Chagas <victorsantoschagas@hotmail.com>
2021-03-23 16:57:16 -0300

    COMMIT Com deploy parcial c heroku

---
+++ ormconfig.js
@@ -0,0 +1,27 @@
+console.log(process.env.DATABASE_URL)
+module.exports = [
+    {
+        "type":"postgres",
+        "url": process.env.DATABASE_URL,
+        "entities": [
+            "./dist/src/modules/**/infra/typeorm/entities/*.ts"
+          ],
+        "migrations":[
+            "./dist/src/shared/infra/typeorm/migrations/*.ts"
+        ],
+        "cli":{
+            "migrationsDir": "./src/shared/typeorm/migrations"
+        }
+    },
+    {
+        "name":"mongo",
+        "type":"mongodb",
+        "host":"localhost",
+        "port":27017,
+        "database" : "mongo_integradordb",
+        "useUnifiedTopology":true,
+        "entities": [
+            "./src/modules/**/infra/typeorm/schemas/*.ts"
+          ]
+    }
+]
